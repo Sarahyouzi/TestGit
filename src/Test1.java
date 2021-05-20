548c130cd77ddfee81113e531c09331d6675bf55
wanru <1533937610@qq.com>
2021-05-20 20:56:08 +0800

    第一次commit 测试项目

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+/**
+ * @Author:WWR
+ * @Date: 2021/5/20- 05- 20- 20:42
+ * @Description:PACKAGE_NAME
+ * @Version:1.0
+ */
+public class Test1 {
+	public static void main(String[] args) {
+
+	}
+}
